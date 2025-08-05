1affd9acb87e3ebb9fa1721e3dad6a9aa75e9562
CuLiBiAkA <97124303+CuLiBiAkA@users.noreply.github.com>
2025-08-05 21:58:58 +0300

    hw01-xml-config-complete

--- hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,33 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
+    public static final String D_S = "%d. %s";
+
     private final IOService ioService;
+
+    private final QuestionDao questionDao;
 
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
-        // Получить вопросы из дао и вывести их с вариантами ответов
+
+        var questions = questionDao.findAll();
+
+        questions.forEach(question -> {
+            ioService.printLine("Question: " + question.text());
+            var answers = question.answers();
+
+            for (int i = 0; i < answers.size(); i++) {
+                ioService.printFormattedLine(D_S, i + 1, answers.get(i).text());
+            }
+
+            ioService.printLine("");
+        });
     }
 }
