01afbfaa4960c71171861b1020097252dc3b7a76
AAV <culibiaka2012@yandex.ru>
2025-09-18 01:18:34 +0200

    hw03-spring-boot-complete

--- hw03-spring-boot/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,12 +1,17 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Component
@@ -15,11 +20,27 @@ public class CsvQuestionDao implements QuestionDao {
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        try (InputStream inputStream = getClass().getClassLoader()
+                .getResourceAsStream(fileNameProvider.getTestFileName());
+             InputStreamReader reader = new InputStreamReader(Objects.requireNonNull(inputStream))) {
 
-        return new ArrayList<>();
+            var dtos = getQuestionDtos(reader);
+
+            return dtos.stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+
+        } catch (Exception e) {
+            throw new QuestionReadException("Failed to read questions from CSV", e);
+        }
+    }
+
+    private List<QuestionDto> getQuestionDtos(InputStreamReader reader) {
+        return new CsvToBeanBuilder<QuestionDto>(reader)
+                .withType(QuestionDto.class)
+                .withSeparator(';')
+                .withSkipLines(1)
+                .build()
+                .parse();
     }
 }
--- hw03-spring-boot/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,6 +3,7 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
@@ -17,17 +18,39 @@ public class TestServiceImpl implements TestService {
     @Override
     public TestResult executeTestFor(Student student) {
         ioService.printLine("");
-        ioService.printLineLocalized("TestService.answer.the.questions");
-        ioService.printLine("");
-
+        ioService.printFormattedLineLocalized("TestService.answer.the.questions");
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
 
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
-            testResult.applyAnswer(question, isAnswerValid);
+        for (var question : questions) {
+            presentQuestion(question);
+            var choice = askUserChoice(question);
+            var isCorrect = evaluateAnswer(question, choice);
+
+            testResult.applyAnswer(question, isCorrect);
         }
         return testResult;
     }
 
+    private void presentQuestion(Question question) {
+        ioService.printLine(question.text());
+        for (int i = 0; i < question.answers().size(); i++) {
+            var answer = question.answers().get(i);
+            ioService.printFormattedLine("%d) %s", i + 1, answer.text());
+        }
+    }
+
+    private int askUserChoice(Question question) {
+        return ioService.readIntForRangeWithPromptLocalized(
+                1,
+                question.answers().size(),
+                "TestService.answer.the.in",
+                "TestService.answer.the.error"
+        );
+    }
+
+    private boolean evaluateAnswer(Question question, int choice) {
+        var chosenAnswer = question.answers().get(choice - 1);
+        return chosenAnswer.isCorrect();
+    }
 }
