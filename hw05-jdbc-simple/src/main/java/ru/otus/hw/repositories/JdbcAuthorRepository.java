675a0a97678ce6e0fc6f1e394aea33d15b388dca
CuLiBiAkA <97124303+CuLiBiAkA@users.noreply.github.com>
2026-01-15 15:53:14 +0200

    hw05-spring-jdbc-complete

--- hw05-jdbc-simple/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
+++ hw05-jdbc-simple/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -1,6 +1,8 @@
 package ru.otus.hw.repositories;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Author;
 
@@ -8,26 +10,50 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class JdbcAuthorRepository implements AuthorRepository {
+
+    private static final AuthorRowMapper AUTHOR_ROW_MAPPER = new AuthorRowMapper();
+
+    private final NamedParameterJdbcOperations jdbcOperations;
 
     @Override
     public List<Author> findAll() {
-        return new ArrayList<>();
+        try {
+            return jdbcOperations.query(
+                    "SELECT id, full_name FROM authors",
+                    AUTHOR_ROW_MAPPER
+            );
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
     }
 
     @Override
     public Optional<Author> findById(long id) {
-        return Optional.empty();
+        try {
+            return jdbcOperations.query(
+                    "SELECT id, full_name FROM authors WHERE id = :id",
+                    Map.of("id", id),
+                    AUTHOR_ROW_MAPPER
+            ).stream().findFirst();
+        } catch (Exception e) {
+            return Optional.empty();
+        }
     }
 
     private static class AuthorRowMapper implements RowMapper<Author> {
 
         @Override
         public Author mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+            return new Author(
+                    rs.getLong("id"),
+                    rs.getString("full_name")
+            );
         }
     }
 }
