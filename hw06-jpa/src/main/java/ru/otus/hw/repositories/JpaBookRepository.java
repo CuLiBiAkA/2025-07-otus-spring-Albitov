b7eaa842e81f707b49099a8b7a79783fbf9821b7
CuLiBiAkA <97124303+CuLiBiAkA@users.noreply.github.com>
2026-01-15 18:17:06 +0200

    hw06-spring-jpa-complete

---
+++ hw06-jpa/src/main/java/ru/otus/hw/repositories/JpaBookRepository.java
@@ -0,0 +1,59 @@
+package ru.otus.hw.repositories;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.models.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class JpaBookRepository implements BookRepository {
+
+    private final EntityManager em;
+
+    @Override
+    public List<Book> findAll() {
+        return em.createQuery("""
+                        select b
+                        from Book b
+                        join fetch b.author
+                        join fetch b.genre
+                        """, Book.class)
+                .getResultList();
+    }
+
+    @Override
+    public Optional<Book> findById(long id) {
+        var result = em.createQuery("""
+                        select b
+                        from Book b
+                        join fetch b.author
+                        join fetch b.genre
+                        where b.id = :id
+                        """, Book.class)
+                .setParameter("id", id)
+                .getResultList();
+
+        return result.stream().findFirst();
+    }
+
+    @Override
+    public Book save(Book book) {
+        if (book.getId() == 0) {
+            em.persist(book);
+            return book;
+        }
+        return em.merge(book);
+    }
+
+    @Override
+    public void deleteById(long id) {
+        var book = em.find(Book.class, id);
+        if (book != null) {
+            em.remove(book);
+        }
+    }
+}
